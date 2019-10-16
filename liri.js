5a88f715bc2703c2c6210105c251b164dc82283c
Eugene Lee <eug703@gmail.com>
2019-10-16 15:36:01 -0400

    More setup files

---
+++ liri.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var spotify = new spotify(keys.spotify;)
+
+var inquirer = require('inquirer');
+var moment = require('moment');
+var axios = require('axios');
+var spotify = require('node-spotify-api');
